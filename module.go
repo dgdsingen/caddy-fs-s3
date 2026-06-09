dc6fbc0f00f0a6fe30dc7bef8643d24c06c83e7e
dgdsingen <dgdsingen@dgdsingenui-MacBookPro.local>
2026-06-09 16:23:09 +0900

    init github.com/dgdsingen/caddy-fs-s3

---
+++ module.go
@@ -0,0 +1,136 @@
+// Package caddyfss3 implements a Caddy virtual filesystem module for AWS S3 (and compatible) object store.
+package caddyfss3
+
+import (
+	"errors"
+	"io/fs"
+	"log/slog"
+
+	"github.com/dgdsingen/caddy-fs-s3/s3fs"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+)
+
+func init() {
+	caddy.RegisterModule(FS{})
+}
+
+// Interface guards
+var (
+	_ fs.StatFS             = (*FS)(nil)
+	_ caddyfile.Unmarshaler = (*FS)(nil)
+)
+
+// FS is a Caddy virtual filesystem module for AWS S3 (and compatible) object store.
+type FS struct {
+	fs.StatFS `json:"-"`
+
+	// The name of the S3 bucket.
+	Bucket string `json:"bucket,omitempty"`
+
+	// The AWS region the bucket is hosted in.
+	Region string `json:"region,omitempty"`
+
+	// The AWS profile to use if multiple profiles are specified.
+	Profile string `json:"profile,omitempty"`
+
+	// Use non-standard endpoint for S3.
+	Endpoint string `json:"endpoint,omitempty"`
+
+	// Set this to `true` to enable the client to use path-style addressing.
+	UsePathStyle bool `json:"use_path_style,omitempty"`
+
+	// DEPRECATED: please use 'use_path_style' instead.
+	// Set this to `true` to force the request to use path-style addressing.
+	S3ForcePathStyle bool `json:"force_path_style,omitempty"`
+}
+
+// CaddyModule returns the Caddy module information.
+func (FS) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "caddy.fs.s3",
+		New: func() caddy.Module { return new(FS) },
+	}
+}
+
+// Provision implements the [caddy.Provisioner] interface.
+func (fs *FS) Provision(ctx caddy.Context) error {
+	if fs.Bucket == "" {
+		return errors.New("bucket must be set")
+	}
+
+	if fs.S3ForcePathStyle {
+		ctx.Logger().Warn("force_path_style is deprecated, please use use_path_style instead")
+	}
+
+	var configOpts []func(*config.LoadOptions) error
+
+	if fs.Region != "" {
+		configOpts = append(configOpts, config.WithRegion(fs.Region))
+	}
+
+	if fs.Profile != "" {
+		configOpts = append(configOpts, config.WithSharedConfigProfile(fs.Profile))
+	}
+
+	cfg, err := config.LoadDefaultConfig(ctx.Context, configOpts...)
+	if err != nil {
+		ctx.Slogger().Error("could not create AWS config", slog.String("error", err.Error()))
+
+		return err
+	}
+
+	client := s3.NewFromConfig(cfg, func(o *s3.Options) {
+		if fs.Endpoint != "" {
+			o.BaseEndpoint = aws.String(fs.Endpoint)
+		}
+		// see https://tsak.dev/posts/aws-sdk-suppress-checksum-warning/
+		o.DisableLogOutputChecksumValidationSkipped = true
+		o.UsePathStyle = fs.UsePathStyle || fs.S3ForcePathStyle
+	})
+
+	// ReadSeeker is required by Caddy
+	fs.StatFS = s3fs.New(client, fs.Bucket, s3fs.WithReadSeeker)
+
+	return nil
+}
+
+// UnmarshalCaddyfile unmarshals a caddyfile.
+func (fs *FS) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	if !d.Next() { // skip block beginning
+		return d.ArgErr()
+	}
+
+	for nesting := d.Nesting(); d.NextBlock(nesting); {
+		switch d.Val() {
+		case "bucket":
+			if !d.AllArgs(&fs.Bucket) {
+				return d.ArgErr()
+			}
+		case "region":
+			if !d.AllArgs(&fs.Region) {
+				return d.ArgErr()
+			}
+		case "profile":
+			if !d.AllArgs(&fs.Profile) {
+				return d.ArgErr()
+			}
+		case "endpoint":
+			if !d.AllArgs(&fs.Endpoint) {
+				return d.ArgErr()
+			}
+		case "use_path_style":
+			fs.UsePathStyle = true
+		case "force_path_style":
+			fs.S3ForcePathStyle = true
+		default:
+			return d.Errf("%s not a valid caddy.fs.s3 option", d.Val())
+		}
+	}
+
+	return nil
+}
---
+++ s3fs/file.go
@@ -0,0 +1,174 @@
+package s3fs
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"io/fs"
+	"net/http"
+	"path"
+	"time"
+
+	awshttp "github.com/aws/aws-sdk-go-v2/aws/transport/http"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+var (
+	_ fs.File     = (*file)(nil)
+	_ fs.FileInfo = (*fileInfo)(nil)
+	_ io.Seeker   = (*file)(nil)
+)
+
+type file struct {
+	cl     Client
+	bucket string
+	name   string
+
+	rc io.ReadCloser
+
+	stat   func() (fs.FileInfo, error)
+	size   int64
+	offset int64
+	eTag   string
+}
+
+func openFile(cl Client, bucket string, name string) (fs.File, error) {
+	head, err := cl.HeadObject(context.Background(), &s3.HeadObjectInput{
+		Bucket: &bucket,
+		Key:    &name,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	var etag string
+	if head.ETag != nil {
+		etag = *head.ETag
+	}
+	size := derefInt64(head.ContentLength)
+	modTime := derefTime(head.LastModified)
+
+	return &file{
+		cl:     cl,
+		bucket: bucket,
+		name:   name,
+		size:   size,
+		offset: 0,
+		eTag:   etag,
+		stat: func() (fs.FileInfo, error) {
+			return &fileInfo{
+				name:    path.Base(name),
+				size:    size,
+				modTime: modTime,
+			}, nil
+		},
+	}, nil
+}
+
+func (f *file) open() error {
+	if f.rc != nil {
+		return nil
+	}
+	if f.offset >= f.size {
+		f.rc = io.NopCloser(eofReader{})
+		return nil
+	}
+
+	in := &s3.GetObjectInput{
+		Bucket: &f.bucket,
+		Key:    &f.name,
+	}
+	if f.offset > 0 {
+		in.Range = ptr(fmt.Sprintf("bytes=%d-", f.offset))
+		if f.eTag != "" {
+			in.IfMatch = &f.eTag
+		}
+	}
+
+	out, err := f.cl.GetObject(context.Background(), in)
+	if err != nil {
+		if e := new(awshttp.ResponseError); errors.As(err, &e) {
+			if e.HTTPStatusCode() == http.StatusPreconditionFailed {
+				return fmt.Errorf("s3fs.file.Seek: file has changed while seeking: %w", fs.ErrNotExist)
+			}
+		}
+		return err
+	}
+
+	f.rc = out.Body
+	return nil
+}
+
+func (f *file) Read(p []byte) (int, error) {
+	if err := f.open(); err != nil {
+		return 0, err
+	}
+	n, err := f.rc.Read(p)
+	f.offset += int64(n)
+	return n, err
+}
+
+func (f *file) Seek(offset int64, whence int) (int64, error) {
+	newOffset := f.offset
+
+	switch whence {
+	case io.SeekStart:
+		newOffset = offset
+	case io.SeekCurrent:
+		newOffset += offset
+	case io.SeekEnd:
+		newOffset = f.size + offset
+	default:
+		return 0, errors.New("s3fs.file.Seek: invalid whence")
+	}
+
+	if newOffset < 0 {
+		return 0, errors.New("s3fs.file.Seek: seeked to a negative position")
+	}
+
+	if newOffset == f.offset {
+		return newOffset, nil
+	}
+
+	if f.rc != nil {
+		if err := f.rc.Close(); err != nil {
+			return f.offset, err
+		}
+		f.rc = nil
+	}
+
+	f.offset = newOffset
+	return f.offset, nil
+}
+
+func (f *file) Stat() (fs.FileInfo, error) { return f.stat() }
+
+func (f *file) Close() error {
+	if f.rc != nil {
+		return f.rc.Close()
+	}
+	return nil
+}
+
+type fileInfo struct {
+	name    string
+	size    int64
+	mode    fs.FileMode
+	modTime time.Time
+}
+
+func (fi fileInfo) Name() string       { return path.Base(fi.name) }
+func (fi fileInfo) Size() int64        { return fi.size }
+func (fi fileInfo) Mode() fs.FileMode  { return fi.mode }
+func (fi fileInfo) ModTime() time.Time { return fi.modTime }
+func (fi fileInfo) IsDir() bool        { return fi.mode.IsDir() }
+func (fi fileInfo) Sys() interface{}   { return nil }
+
+type eofReader struct{}
+
+func (eofReader) Read([]byte) (int, error) { return 0, io.EOF }
+
+func ptr[T any](v T) *T {
+	return &v
+}
